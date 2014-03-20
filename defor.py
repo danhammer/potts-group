01151aae3032c91f5f60a3b8d7e27ef78e987122
Dan Hammer <dan.s.hammer@gmail.com>
2014-03-20 09:35:26 -0700

    working code for one ecoregion

--- defor.py
+++ defor.py
@@ -4,7 +4,6 @@ import json
 import itertools
 
 import pandas as pd
-import matplotlib.pyplot as plt
 
 from osgeo import ogr
 
@@ -30,24 +29,24 @@ def _poly_loss(poly, year):
     coords = poly.ExportToJson()
     params = {'geom': coords, 'begin': year - 1, 'end': year}
     url = _get_url(params)
-    f = urllib.urlopen(url)
-    return json.load(f)['loss']
+    res = urllib.urlopen(url)
+    return json.load(res)['loss']
 
-def _get_loss(shp_path, year, prov, subprov):
+def _get_loss(layer, year, subprov):
     """Returns the year's forest cover loss for the supplied province
     and subprovince within the given shapefile path.
 
     Args:
-      shp_path: string path relative to buffer directory
+      layer: OGR layer instance <osgeo.ogr.Layer> for a single province
       year: integer value for the year, with 2001 <= year <= 2012
       prov: string province name (associated with GADM level 1)
       subprov: string subprovince name (associated with GADM level 2)
       
     """
-    driver = ogr.GetDriverByName("ESRI Shapefile")
-    dataSource = driver.Open(shp_path, 0)
-    layer = dataSource.GetLayer()
-    layer.SetAttributeFilter("name_1 = '%s'" % prov)
+    # driver = ogr.GetDriverByName("ESRI Shapefile")
+    # dataSource = driver.Open(shp_path, 0)
+    # layer = dataSource.GetLayer()
+    # layer.SetAttributeFilter("name_1 = '%s'" % prov)
 
     res = []
     # There may be multiple polygons for the given subprovince and GEE
@@ -64,14 +63,35 @@ def _get_loss(shp_path, year, prov, subprov):
     return {'prov':prov, 'subprov':subprov, 'year':year, 'loss':sum(flattened)}
 
 
-def _plot_loss(prov='Jawa Timur', subprov='Gresik'):
-    """Plots the line graph of UMD forest cover loss for the supplied
-    Indonesian province and subprovince
+idn = "data/idn_simple.shp"
 
-    """
-    idn = "data/idn_simple.shp"
-    ts = [_get_loss(idn, yr, prov, subprov) for yr in range(2001, 2013)]
-    df = pd.DataFrame(ts)
-    plt.plot(df['year'], df['loss'])
-    plt.show()
-    return df
+def _list_subprovs(layer_instance, prov):
+    # layer_instance.SetAttributeFilter("name_1 = '%s'" % prov)
+    return [feature.GetField("name_2") for feature in layer_instance]
+    
+
+def _res_dict(prov_layer, subprov_name):
+    return [_get_loss(prov_layer, yr, subprov_name) for yr in range(2001, 2013)]
+
+def _prov_dataframe(shp_path, prov = 'Jawa Timur'):
+    driver = ogr.GetDriverByName("ESRI Shapefile")
+    dataSource = driver.Open(shp_path, 0)
+    layer = dataSource.GetLayer()
+    layer.SetAttributeFilter("name_1 = '%s'" % prov)
+    subprovs = ['Banyuwangi', 'Batu']
+    return [_res_dict(layer, sub) for sub in subprovs]
+    # [_res_dict(layer, sub) for sub in _list_subprovs(layer, prov)]
+
+
+
+# def _plot_loss(prov='Jawa Timur', subprov='Gresik'):
+#     """Plots the line graph of UMD forest cover loss for the supplied
+#     Indonesian province and subprovince
+
+#     """
+#     idn = "data/idn_simple.shp"
+#     ts = [_get_loss(idn, yr, subprov) for yr in range(2001, 2013)]
+#     df = pd.DataFrame(ts)
+#     plt.plot(df['year'], df['loss'])
+#     plt.show()
+#     return df
