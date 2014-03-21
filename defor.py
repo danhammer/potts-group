e1f96f1aaefed77c627c285a6dd641d575733d2b
Dan Hammer <dan.s.hammer@gmail.com>
2014-03-20 21:12:33 -0700

    started a simplification

--- defor.py
+++ defor.py
@@ -4,9 +4,29 @@ import json
 import itertools
 
 import pandas as pd
-
 from osgeo import ogr
 
+# Go from JSON to a measurement of UMD deforestation by year.  If you
+# have a shapefile, you will have to convert and simplify the
+# polygons, preserving topology.  The Earth Engine API does not
+# support long, long requests.  You can convert the shapefile using
+# OGR within the data subdirectory:
+
+# ogr2ogr -f GeoJSON map.json map.shp -simplify 0.001
+
+# You can also upload your various formats (csv, shp, kml, etc.) to
+# CartoDB and export to JSON.  Regardless of the path you choose,
+# young Padawan, JSON is the destination.  Specifically,
+# data/map.json.
+
+def _read_data(data = 'data/map.json'):
+    """Returns a list of JSON dictionaries, one for each feature of
+    the polygons.
+
+    """
+    with open(data) as json_file:
+        x = json.load(json_file)
+        return x['features']
 
 def _get_url(params = {}):
     """Returns the string URL given the supplied parameters to hit the
@@ -17,81 +37,26 @@ def _get_url(params = {}):
     return '%s?%s' % (endpoint, urllib.urlencode(params))
 
 
-def _poly_loss(poly, year):
-    """Returns the UMD forest cover loss for the polygon in the given
-    year, relative to the previous year.
-      
-    Args:
-      poly: gdal polygon instance
-      year: integer value for the year, with 2001 <= year <= 2012
+def _polygon(coords):
+    return {"type": "Polygon", "coordinates": coords}
 
-    """
-    coords = poly.ExportToJson()
-    params = {'geom': coords, 'begin': year - 1, 'end': year}
-    url = _get_url(params)
+def _params(coords, year):
+    p = _polygon(coords)
+    return {"begin":year-1, "end":year, "geom":p}
+
+def _grab_loss(coords, year, name_dict = {}):
+    url = _get_url(_params(coords, year))
     res = urllib.urlopen(url)
-    return json.load(res)['loss']
+    return res.read()
 
-def _get_loss(layer, year, subprov):
-    """Returns the year's forest cover loss for the supplied province
-    and subprovince within the given shapefile path.
-
-    Args:
-      layer: OGR layer instance <osgeo.ogr.Layer> for a single province
-      year: integer value for the year, with 2001 <= year <= 2012
-      prov: string province name (associated with GADM level 1)
-      subprov: string subprovince name (associated with GADM level 2)
-      
-    """
-    # driver = ogr.GetDriverByName("ESRI Shapefile")
-    # dataSource = driver.Open(shp_path, 0)
-    # layer = dataSource.GetLayer()
-    # layer.SetAttributeFilter("name_1 = '%s'" % prov)
+def _process_entry(json_entry):
+    name_1 = json_entry['properties']['NAME_1']
+    name_2 = json_entry['properties']['NAME_2']
+    coord_set = json_entry['geometry']['coordinates']
 
     res = []
-    # There may be multiple polygons for the given subprovince and GEE
-    # only accepts calls for polygons, not multipolygons. Do one API
-    # call each.
-    for feature in layer:
-        if feature.GetField("name_2") == subprov:
-            geom = feature.GetGeometryRef()
-            # geom is a multipolygon, 
-            x = [_poly_loss(g, year) for g in geom]
-            res.append(x)
+    for coord in coord_set:
+       x = [_grab_loss(coord, yr) for yr in range(2001,2013)]
+       res.append(x)
 
-    flattened = list(itertools.chain(*res))
-    return {'prov':prov, 'subprov':subprov, 'year':year, 'loss':sum(flattened)}
-
-
-idn = "data/idn_simple.shp"
-
-def _list_subprovs(layer_instance, prov):
-    # layer_instance.SetAttributeFilter("name_1 = '%s'" % prov)
-    return [feature.GetField("name_2") for feature in layer_instance]
-    
-
-def _res_dict(prov_layer, subprov_name):
-    return [_get_loss(prov_layer, yr, subprov_name) for yr in range(2001, 2013)]
-
-def _prov_dataframe(shp_path, prov = 'Jawa Timur'):
-    driver = ogr.GetDriverByName("ESRI Shapefile")
-    dataSource = driver.Open(shp_path, 0)
-    layer = dataSource.GetLayer()
-    layer.SetAttributeFilter("name_1 = '%s'" % prov)
-    subprovs = ['Banyuwangi', 'Batu']
-    return [_res_dict(layer, sub) for sub in subprovs]
-    # [_res_dict(layer, sub) for sub in _list_subprovs(layer, prov)]
-
-
-
-# def _plot_loss(prov='Jawa Timur', subprov='Gresik'):
-#     """Plots the line graph of UMD forest cover loss for the supplied
-#     Indonesian province and subprovince
-
-#     """
-#     idn = "data/idn_simple.shp"
-#     ts = [_get_loss(idn, yr, subprov) for yr in range(2001, 2013)]
-#     df = pd.DataFrame(ts)
-#     plt.plot(df['year'], df['loss'])
-#     plt.show()
-#     return df
+    return res
